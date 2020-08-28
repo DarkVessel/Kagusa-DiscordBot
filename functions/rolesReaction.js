d86969936c0e28e2eae8daa7eab5801f6ee85f87
DarkVessel <rs5334819@gmail.com>
2020-08-28 21:56:31 +0400

    Fix

--- functions/rolesReaction.js
+++ functions/rolesReaction.js
@@ -6,16 +6,20 @@ for (const msgID in reactions.msg) {
 };
 
 global.bot.on("messageReactionAdd", (r, user) => {
-    const _a = (r.emoji.id in reactions.r) ? "id" : (r.emoji.name in reactions.r) ? "name" : "";
-    if (!(r.message.id in reactions.msg) || !_a) return;
+    const reactionMsg = reactions.msg[r.message.id];
+    if (!reactionMsg) return;
+    const _a = (r.emoji.id in reactionMsg) ? "id" : (r.emoji.name in reactionMsg) ? "name" : "";
+    if (!_a) return;
     const member = r.message.guild.member(user.id);
-    const role = reactions.msg[r.message.id][r.emoji[_a]];
+    const role = reactionMsg[r.emoji[_a]];
     if (role) member.roles.add(role).catch(err => err);
 });
 global.bot.on("messageReactionRemove", (r, user) => {
-    const _a = (r.emoji.id in reactions.r) ? "id" : (r.emoji.name in reactions.r) ? "name" : "";
-    if (!(r.message.id in reactions.msg) || !_a) return;
+    const reactionMsg = reactions.msg[r.message.id];
+    if (!reactionMsg) return;
+    const _a = (r.emoji.id in reactionMsg) ? "id" : (r.emoji.name in reactionMsg) ? "name" : "";
+    if (!_a) return;
     const member = r.message.guild.member(user.id);
-    const role = reactions.msg[r.message.id][r.emoji[_a]];
+    const role = reactionMsg[r.emoji[_a]];
     if (role) member.roles.remove(role).catch(err => err);
 });
