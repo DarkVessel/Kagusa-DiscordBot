1b6aa22aaa7a58c0bd564c146121d104c1f50548
DarkVessel <48821354+DarkVessel@users.noreply.github.com>
2020-12-05 12:18:44 +0400

    Update guildMemberAdd.js

--- functions/guildMemberAdd.js
+++ functions/guildMemberAdd.js
@@ -1,2 +1,2 @@
-const { rolesAdd } = require("../config.js");
+const { rolesAdd } = require("../../config.js");
 global.bot.on("guildMemberAdd", member => rolesAdd.filter(r => !member.roles.cache.has(r)).forEach(r => member.roles.add(r)));
